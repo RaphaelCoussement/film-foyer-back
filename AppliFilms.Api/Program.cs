9df2e4c680a4a6655007f2c362efc7f80b7867a4
Raphael COUSSEMENT <raphael.coussement@diiage.org>
2025-10-04 18:09:47 +0200

    CORS

--- AppliFilms.Api/Program.cs
+++ AppliFilms.Api/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 builder.Services.AddScoped<IRequestRepository, RequestRepository>();
 builder.Services.AddScoped<IApprovalRepository, ApprovalRepository>();
 
-
 // DI - Services
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddHttpClient<IMovieService, MovieService>();
@@ -46,12 +45,25 @@ builder.Services.AddAuthentication(options =>
         };
     });
 
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Controllers / Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 app.UseAuthorization();
