c5cda974df6eeac4231af55016c22e5b001789e9
Raphael COUSSEMENT <raphael.coussement@diiage.org>
2025-10-05 13:43:51 +0200

    guid

--- AppliFilms.Api/DTOs/Requests/CreateRequestDto.cs
+++ AppliFilms.Api/DTOs/Requests/CreateRequestDto.cs
@@ -1,7 +1,11 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace AppliFilms.Api.DTOs.Requests
 {
     public class CreateRequestDto
     {
+        [BsonRepresentation(BsonType.String)]
         public Guid MovieId { get; set; }
     }
 }
